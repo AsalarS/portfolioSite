58859b538fe204cc2ad379f22fc70463289e6a2a
Ali <a03n55a@gmail.com>
2024-06-10 01:42:06 +0300

    added top bar, fixed links and, fixed heights

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const track = document.getElementById("imageCarousel")
+window.addEventListener('DOMContentLoaded', event => {
 
-window.onmousedown = e => {
-    track.dataset.mousedownAt = e.clientX;
-}
+    // Activate Bootstrap scrollspy on the main nav element
+    const navBar = document.body.querySelector('#navBar');
+    if (navBar) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#navBar',
+            rootMargin: '0px 0px -40%',
+        });
+    };
 
-window.onmouseup = () => {
-    track.dataset.mousedownAt = "0"
-    track.dataset.prevPercentage = track.dataset.percentage;
-}
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 
-window.onmousemove = e => {
-    if(track.dataset.mousedownAt == "0") return;
+});
 
-    const mouseDelta = parseFloat(track.dataset.mousedownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-    const percentage = (mouseDelta / maxDelta) * -100;
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-    
-    track.dataset.percentage = nextPercentage;
 
-    track.style.transform = 'translate(${nextPercentage}%, -50%)';
-}
+// When the user scrolls down 50px from the top of the document, show the nav bar
+window.onscroll = function() {
+    var navBar = document.getElementById("navBar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navBar.classList.add("shown");
+    } else {
+        navBar.classList.remove("shown");
+    }
+};
